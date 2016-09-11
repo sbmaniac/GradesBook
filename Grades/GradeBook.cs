a684adefaadd3a29e44e033dee1febac462b7807
sbmaniac <sbmaniac@gmail.com>
2016-09-11 17:41:19 +0200

    Properties test

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -9,7 +9,18 @@ namespace Grades
    public class GradeBook
     {
        private List<float> grades;
-       public string Name;
+       private string _name;
+
+       public string Name
+        {
+           get { return _name; }
+           set
+           {
+               if (!String.IsNullOrEmpty(value))
+               {
+                   _name = value;
+               }   
+           } }
 
 
         public GradeBook()
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -12,16 +12,25 @@ namespace Grades
         {
             
             GradeBook book = new GradeBook();
+            book.Name = "Maurycy's Grade Book";
+            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine( stats.HighestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
             Console.WriteLine(stats.LowestGrade);
-            
       }
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": "+ result);
+        }
     }
 }
